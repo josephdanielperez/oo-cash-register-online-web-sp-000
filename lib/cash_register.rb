abec5054d8010bedee402ab6fcf10c0e53fdf72c
Joseph Perez <hello@josephdanielperez.com>
2020-07-28 01:20:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
 
 attr_accessor :total, :discount
 
-@@CART = []
+@@CART = {}
 
   def initialize(discount = 0)
     @total = 0
@@ -10,8 +10,13 @@ attr_accessor :total, :discount
   end
 
   def add_item(title, price, quantity = 1)
-    @@CART << title[quantity]
-    @total = @total + price
+    item_info = {}
+    item_info[:name] = title
+    item_info[:price] = price
+    item_info[:quantity] = quantity
+
+    @@CART << item_info
+    @total = @total + price * quantity
   end
 
 end
